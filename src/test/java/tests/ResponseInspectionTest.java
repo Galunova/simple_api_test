aec5d04f0430d68ac17b3c767f4889c1874bf8f6
Adelina Galunova <adelina.vitamine@gmail.com>
2019-12-06 20:40:38 +0600

    Add comment for etag endpoint

--- src/test/java/tests/ResponseInspectionTest.java
+++ src/test/java/tests/ResponseInspectionTest.java
@@ -9,11 +9,13 @@ import org.apache.http.HttpStatus;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ResponseInspectionTest {
     private RequestSpecification baseSpec() {
         return new RequestSpecBuilder()
                 .setContentType(ContentType.JSON.withCharset("UTF-8"))
+//                .setContentType(ContentType.XML)
                 .setBaseUri("http://httpbin.org/")
                 .setPort(80)
                 .addFilter(new ResponseLoggingFilter())
@@ -25,8 +27,52 @@ public class ResponseInspectionTest {
     public void test01_ResponseInspectionCache_Success() {
         given()
                 .spec(baseSpec())
+                .header("If-Modified-Since", "1")
                 .when().get("/cache")
                 .then().assertThat()
+                .statusCode(HttpStatus.SC_NOT_MODIFIED);
+    }
+
+    @Test
+    public void test02_ResponseInspectationCacheValue_Success() {
+        given()
+                .spec(baseSpec())
+                .pathParam("value", "13")
+                .when().get("/cache/{value}")
+                .then().assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void test03_ResponseInspectationCacheEtag_Success() {
+     given()
+     .spec(baseSpec())
+             //TODO I have no idea how it should work!
+     .header("If-None-Match", "686897696a7")
+     .when().get("/etag")
+     .then().assertThat()
+     .statusCode(HttpStatus.SC_PRECONDITION_FAILED);
+    }
+
+    @Test
+    public void test04_ResponseInspectationResponseHeadersGet_Success() {
+        given()
+                .spec(baseSpec())
+                .queryParam("freeform", "ololo")
+                .when().post("/response-headers")
+                .then()
+                .body("freeform", equalTo("ololo"))
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void test05_ResponseInspectationResponseHeadersPost_Success() {
+        given()
+                .spec(baseSpec())
+                .queryParam("freeform", "ololo")
+                .when().post("/response-headers")
+                .then()
+                .body("freeform", equalTo("ololo"))
                 .statusCode(HttpStatus.SC_OK);
     }
 }
